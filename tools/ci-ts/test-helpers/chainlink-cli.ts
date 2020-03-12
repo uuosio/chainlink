67e088fdc0aea74da6f46df92e095114029d5f7f
Ryan Hall <hall.ryan.r@gmail.com>
2020-03-12 14:11:54 -0600

    cleanup chainlink-cli

--- tools/ci-ts/test-helpers/chainlink-cli.ts
+++ tools/ci-ts/test-helpers/chainlink-cli.ts
@@ -4,29 +4,6 @@ import { JobSpec, JobRun } from '../../../operator_ui/@types/operator_ui'
 
 const API_CREDENTIALS_PATH = '/run/secrets/apicredentials'
 
-// export function login(): void {
-//   execa.sync('chainlink', ['admin', 'login', '--file', API_CREDENTIALS_PATH])
-// }
-
-// export function getJobs(): JobSpec[] {
-//   const { stdout } = execa.sync('chainlink', ['-j', 'jobs', 'list'])
-//   return JSON.parse(stdout) as JobSpec[]
-// }
-
-// export function getJobRuns(): JobRun[] {
-//   const { stdout } = execa.sync('chainlink', ['-j', 'runs', 'list'])
-//   return JSON.parse(stdout) as JobRun[]
-// }
-
-// export function createJob(jobSpec: string): JobSpec {
-//   const { stdout } = execa.sync('chainlink', ['-j', 'jobs', 'create', jobSpec])
-//   return JSON.parse(stdout) as JobSpec
-// }
-
-// export function archiveJob(jobId: string): void {
-//   execa.sync('chainlink', ['-j', 'jobs', 'archive', jobId])
-// }
-
 /**
  * interface for the data describing the CL node's keys
  */
@@ -48,6 +25,9 @@ export default class ChainlinkClient {
   }
 
   execute(command: string): object {
+    if (!this.chainlinkURL) {
+      throw Error('no chainlink node URL set')
+    }
     const { stdout } = execa.sync('chainlink', ['-j', ...command.split(' ')])
     return stdout ? JSON.parse(stdout) : null
   }
