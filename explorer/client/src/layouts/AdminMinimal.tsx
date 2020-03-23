f705e0a3e696af9e53b022ee2ec3e3337ff3415d
henrynguyen5 <6404866+HenryNguyen5@users.noreply.github.com>
2020-03-23 19:29:56 -0400

    Add version data to admin page

--- explorer/client/src/layouts/AdminMinimal.tsx
+++ explorer/client/src/layouts/AdminMinimal.tsx
@@ -1,7 +1,8 @@
-import React from 'react'
-import { RouteComponentProps } from '@reach/router'
-import { createStyles, withStyles, WithStyles } from '@material-ui/core/styles'
 import Grid from '@material-ui/core/Grid'
+import { createStyles, withStyles, WithStyles } from '@material-ui/core/styles'
+import { RouteComponentProps } from '@reach/router'
+import React from 'react'
+import Footer from '../components/Admin/Operators/Footer'
 
 const styles = () =>
   createStyles({
@@ -14,16 +15,19 @@ interface Props extends RouteComponentProps, WithStyles<typeof styles> {}
 
 export const AdminMinimal: React.FC<Props> = ({ children, classes }) => {
   return (
-    <Grid
-      container
-      spacing={24}
-      alignItems="center"
-      className={classes.container}
-    >
-      <Grid item xs={12}>
-        <main>{children}</main>
+    <>
+      <Grid
+        container
+        spacing={24}
+        alignItems="center"
+        className={classes.container}
+      >
+        <Grid item xs={12}>
+          <main>{children}</main>
+        </Grid>
       </Grid>
-    </Grid>
+      <Footer />
+    </>
   )
 }
 
